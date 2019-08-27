f0a7cdb3c7f3a96c36deb9640044cac1e54951c6
Crystal Chung <crystal_chung@intuit.com>
2019-08-27 15:01:32 -0700

    Initial Commit

---
+++ LootLogger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LootLogger
+//
+//  Created by Chung, Crystal on 8/27/19.
+//  Copyright © 2019 Chung, Crystal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
